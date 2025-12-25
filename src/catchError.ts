c5be62264df2ffa88096ca0523870fe63fe49d3a
Mariusz Jacek <marios.yaceg@gmail.com>
2025-12-25 19:39:58 +0100

    errorHandler

---
+++ src/catchError.ts
@@ -0,0 +1,22 @@
+import type { Request, Response,  NextFunction } from "express"
+import { ErrorCodes, HttpException } from "./exceptions";
+import { InternalException } from "./exceptions/internalException";
+
+export const catchError = (controller: Function) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        try{
+            await controller(req, res, next);
+        }catch(error: any){
+
+            let exception: HttpException;
+
+            if( error instanceof HttpException){
+                exception = error;
+            }else{
+                exception = new InternalException("Internal server error", error, ErrorCodes.INTERNAL_SERVER_ERROR);
+            }
+
+            next(exception);
+        }
+    }
+}
