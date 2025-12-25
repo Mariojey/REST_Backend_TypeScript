e336c3d188d67e248f6a500a68370e0227fdc984
Mariusz Jacek <marios.yaceg@gmail.com>
2025-12-25 19:38:21 +0100

    prisma  config files

---
+++ src/prisma.ts
@@ -0,0 +1,12 @@
+import "dotenv/config";
+
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client";
+import { SignUpSchema } from "./schema/users";
+
+const connectionString = process.env.DATABASE_URL as string;
+
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter});
+
+export { prisma };
