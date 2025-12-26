3857a5936606914b7dfad281d0e0099212ee187d
Mariusz Jacek <marios.yaceg@gmail.com>
2025-12-26 19:27:18 +0100

    Items CRUD

--- src/controllers/itemController.ts
+++ src/controllers/itemController.ts
@@ -1,5 +1,7 @@
 import type { Request, Response } from "express";
 import { prisma } from "../prisma";
+import { NotFoundError } from "../exceptions/notFound";
+import { ErrorCodes } from "../exceptions";
 
 export const createItemController = async (req: Request, res: Response) => {
 
@@ -10,6 +12,93 @@ export const createItemController = async (req: Request, res: Response) => {
         }
     })
 
+    await prisma.$disconnect();
+
     res.json(item);
 
 };
+
+export const getItemsController = async (req: Request, res: Response) => {
+    
+    const count = await prisma.item.count();
+
+    const items = await prisma.item.findMany({
+        skip : Number(req.query.skip) || 0,
+        take : Number(req.query.take) || 10
+    });
+
+    await prisma.$disconnect();
+
+    res.json({ items, count });
+
+
+}
+
+export const getItemByIdController = async (req: Request, res: Response) => {
+
+    try{
+        const item = await prisma.item.findUnique({
+            where: {
+                id: Number(req.params.id)
+            }
+        });
+
+        await prisma.$disconnect();
+
+        if(!item){
+            throw new NotFoundError("Item not found", ErrorCodes.RESOURCE_NOT_FOUND);
+        }
+        res.json(item);
+
+    }catch(error){
+        throw new NotFoundError("Items not found", ErrorCodes.RESOURCE_NOT_FOUND);
+    }
+
+}
+
+export const updateItemController = async (req: Request, res: Response) => {
+
+    try{
+
+        const item = req.body;
+
+        if(item.tags){
+            item.tags = item.tags.join(",");
+        }
+
+        const updatedItem = await prisma.item.update({
+            where: {
+                id: Number(req.params.id)
+            },
+            data: item
+        })
+
+        await prisma.$disconnect();
+
+        res.json(updatedItem);
+
+    }catch(error){
+        throw new NotFoundError("Items not found", ErrorCodes.RESOURCE_NOT_FOUND);
+    }
+
+}
+
+export const deleteItemController = async (req: Request, res: Response) => {
+
+    try{
+
+        const deletedItem = await prisma.item.delete({
+            where: {
+                id: Number(req.params.id)
+            }
+        });
+
+        await prisma.$disconnect();
+
+        res.json(deletedItem);
+
+    }catch(error){
+        throw new NotFoundError("Items not found", ErrorCodes.RESOURCE_NOT_FOUND);
+    }
+
+}
--- src/routes/itemRouter.ts
+++ src/routes/itemRouter.ts
@@ -1,11 +1,19 @@
 import { Router } from "express";
 import { catchError } from "../catchError";
-import { createItemController } from "../controllers/itemController";
+import { createItemController, deleteItemController, getItemByIdController, getItemsController, updateItemController } from "../controllers/itemController";
 import { authMiddleware } from "../middlewares/authMiddleware";
 import { adminMiddleware } from "../middlewares/adminMiddleware";
 
 const itemRouter:Router = Router();
 
-itemRouter.post('/',[authMiddleware,adminMiddleware] , catchError(createItemController))
+itemRouter.post('/',[authMiddleware,adminMiddleware] , catchError(createItemController));
+
+itemRouter.get('/', [authMiddleware, adminMiddleware], catchError(getItemsController));
+
+itemRouter.get('/:id', [authMiddleware, adminMiddleware], catchError(getItemByIdController));
+
+itemRouter.put('/:id', [authMiddleware, adminMiddleware], catchError(updateItemController));
+
+itemRouter.delete('/:id', [authMiddleware, adminMiddleware], catchError(deleteItemController));
 
 export default itemRouter;
