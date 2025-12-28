baa9879f374746f8df1babc39911c74cdfb8f14f
Mariusz Jacek <marios.yaceg@gmail.com>
2025-12-28 17:17:14 +0100

    updateUser localization controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,9 +1,71 @@
 import type { Request, Response, NextFunction } from 'express';
-import { LocalizationSchema } from '../schema/users';
+import { LocalizationSchema, UpdateUserSchema } from '../schema/users';
 import { prisma } from '../prisma';
 import { NotFoundError } from '../exceptions/notFound';
 import { ErrorCodes } from '../exceptions';
+import { Localization } from '../../generated/prisma/client';
+import { BadRequestError } from '../exceptions/badRequest';
 
+export const updateUserController = async (req: Request, res: Response) => {
+
+    const dataToUse = UpdateUserSchema.parse(req.body);
+
+    let shippingLocalization : Localization;
+    let billingLocalization : Localization;
+
+    if(dataToUse.defaultShippingLocalization){
+
+        try{
+
+            shippingLocalization = await prisma.localization.findFirstOrThrow({
+
+                where: {
+                    id: dataToUse.defaultShippingLocalization
+                }
+
+            })
+        }catch(error){
+
+            throw new NotFoundError("Localization not found", ErrorCodes.RESOURCE_NOT_FOUND);
+        
+        }
+
+        if(shippingLocalization.userId !== req.user.id) {
+            throw new BadRequestError("Shipping localization does not belong to the user", ErrorCodes.VALIDATION_ERROR);
+        }
+
+    }
+
+    if(dataToUse.defaultBillingLocalization){
+        try{
+            billingLocalization = await prisma.localization.findFirstOrThrow({
+                where: {
+                    id: dataToUse.defaultBillingLocalization
+                }
+            })
+            if(billingLocalization.userId !== req.user.id) {
+                throw new BadRequestError("Billing localization does not belong to the user", ErrorCodes.VALIDATION_ERROR);
+            }
+        }catch(error){
+            throw new NotFoundError("Localization not found", ErrorCodes.RESOURCE_NOT_FOUND);
+        }
+
+        if(billingLocalization.userId !== req.user.id) {
+            throw new BadRequestError("Billing localization does not belong to the user", ErrorCodes.VALIDATION_ERROR);
+        }
+    }
+
+    const updatedUser = await prisma.user.update({
+        where: {
+            id: req.user.id
+        },
+        data: dataToUse,
+    })
+
+    await prisma.$disconnect();
+
+    res.json(updatedUser);
+}
 
 export const getLocalizationsController = async (req: Request, res: Response) => {
 
